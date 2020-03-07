b3f362225f8c168b14d014a908293ba8e05e0c19
labi23 <michal.labedzki@gmail.com>
2020-03-07 23:01:05 +0100

    change api kay

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    config: {
+      apiKey: 'AIzaSyBW9ewltT0C8NbRv1j46xuq-ZmWwfo_J4k',
+      authDomain: 'micro-shoreline-263716.firebaseapp.com',
+      databaseURL: 'https://micro-shoreline-263716.firebaseio.com',
+      projectId: 'micro-shoreline-263716',
+      storageBucket: 'micro-shoreline-263716.appspot.com',
+      messagingSenderId: '943204967249',
+      appId: '1:943204967249:web:a8313f4a507c2926016970',
+      measurementId: 'G-ST85Y6TC25'
+    }
+  };
